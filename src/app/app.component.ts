5fb66bee66b0beac04efa41ef0ebe75f65d74ab1
Inmaculada <icuemau0909@iesmartinezm.es>
2022-01-26 10:50:26 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'listTareas';
+  todo = ''
+  tarea: any[] = []
+  addTodo() {
+    if(this.todo != ''){
+      this.tarea = [...this.tarea, this.todo]//crea un nuevo array con lo que tuviera this.tarea+this.todo
+      this.todo = ''
+      }
+  }
+  delTodo(todoCompletado: number){
+    /*this.tarea = this.tarea.filter(todo => todo != todoCompletado)*/
+    this.tarea.splice(todoCompletado,1)
+  }
+
 }
